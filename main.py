5dc9298b9a01f156de33de75d0698e9d9d8213c3
itency <itency@outlook.com>
2024-07-01 15:08:04 +0300

    add functions get_num_words and get_char_text

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+    char_text = get_char_text(text)
+    print(char_text)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_text(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
