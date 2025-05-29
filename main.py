21d22d747ed2e9a9c5befbe9db5429a580634237
ItencY <itency@outlook.com>
2025-05-29 12:00:00 +0300

    count characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import number_of_words
+from stats import get_count_character
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,4 +12,6 @@ def main():
     print(text)
     num_words = number_of_words(text)
     print(num_words)
+    count_char = get_count_character(text)
+    print(count_char)
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def number_of_words(text):
     split_text = text.split()
     num_words = len(split_text)
     return f"{num_words} words found in the document"
+
+def get_count_character(text):
+    char_counts = {}
+    for char in text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
