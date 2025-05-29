5ef6b43556ed13ecc2379f0613b562a18377034d
ItencY <itency@outlook.com>
2025-05-29 12:48:01 +0300

    add sys.arg in project and add some books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import number_of_words
 from stats import get_count_character
 from stats import sort_char_counts
@@ -9,7 +10,10 @@ def get_book_text(filepath):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     num_words = number_of_words(text)
     count_char = get_count_character(text)
     sort_char = sort_char_counts(count_char)
