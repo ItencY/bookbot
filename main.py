48fea873898ac0e47a51d34dc382c41e9921e311
ItencY <itency@outlook.com>
2025-07-22 10:32:57 +0300

    Create func for read book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_content = f.read()
+    return file_content
+
+def main():
+    path_book = "books/frankenstein.txt"
+    print(get_book_text(path_book))
+
+main()
