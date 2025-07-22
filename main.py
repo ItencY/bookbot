24aacd082db74eaa398ad921e94790cb6539f0f8
ItencY <itency@outlook.com>
2025-07-22 14:13:20 +0300

    Create func for get char counters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_number_of_words
+from stats import get_count_characters
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,5 +10,7 @@ def main():
     path_book = "books/frankenstein.txt"
     book_text = get_book_text(path_book)
     count_words = get_number_of_words(book_text)
+    chars_dict = get_count_characters(book_text)
     print(f"{count_words} words found in the document")
+    print(chars_dict)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_number_of_words(book_text):
     words = book_text.split()
     return len(words)
+
+def get_count_characters(book_text):
+    chars = {}
+    for char in book_text:
+        lower_char = char.lower()
+        if lower_char in chars:
+            chars[lower_char] = chars[lower_char] + 1
+        else:
+            chars[lower_char] = 1
+    return chars
