05cc9ffabcdae8a167c49854c05d67dcb00cb5f1
ItencY <itency@outlook.com>
2025-07-22 13:47:46 +0300

    Refactoring code

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from stats import get_number_of_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_content = f.read()
     return file_content
 
-def number_of_words(book_text):
-    text = get_book_text(book_text)
-    words = text.split()
-    count_words = len(words)
-    return f"{count_words} words found in the document"
-
 def main():
     path_book = "books/frankenstein.txt"
-    print(get_book_text(path_book))
-    print(number_of_words(path_book))
+    book_text = get_book_text(path_book)
+    count_words = get_number_of_words(book_text)
+    print(f"{count_words} words found in the document")
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_number_of_words(book_text):
+    words = book_text.split()
+    return len(words)
