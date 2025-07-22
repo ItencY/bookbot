843cb78136f7f3a562f4b17ccd2a7fcb7acd6a21
ItencY <itency@outlook.com>
2025-07-22 10:53:26 +0300

    Create func for count of words in the string

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(path_to_file):
         file_content = f.read()
     return file_content
 
+def number_of_words(book_text):
+    text = get_book_text(book_text)
+    words = text.split()
+    count_words = len(words)
+    return f"{count_words} words found in the document"
+
 def main():
     path_book = "books/frankenstein.txt"
     print(get_book_text(path_book))
-
+    print(number_of_words(path_book))
 main()
