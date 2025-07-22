8f41c8a38bcf4bef209437b35c353f149e51d531
ItencY <itency@outlook.com>
2025-07-22 17:09:43 +0300

    Adding and change path_book to sys arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_number_of_words
 from stats import get_count_characters
 from stats import sorted_dict
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,12 +9,15 @@ def get_book_text(path_to_file):
     return file_content
 
 def main():
-    path_book = "books/frankenstein.txt"
-    book_text = get_book_text(path_book)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    book_text = get_book_text(book)
     count_words = get_number_of_words(book_text)
     chars_dict = get_count_characters(book_text)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path_book}...")
+    print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words} total words")
     print("--------- Character Count -------")
