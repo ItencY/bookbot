79ad304ccf6b69d97018dc8088757c49cc067c28
ItencY <itency@outlook.com>
2025-09-25 10:08:13 +0300

    Create func get_num_words

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def get_book_text(path):
     with open(path) as f:
-        file_content = f.read()
-    return file_content
+        return f.read()
+
+def get_num_words(text):
+    split_words = text.split()
+    return len(split_words)
 
 def main():
     path_to_file = "./books/frankenstein.txt"
     read_file = get_book_text(path_to_file)
-    print(read_file)
-
-
+    num_words = get_num_words(read_file)
+    print(f"Found {num_words} total words")
 main()
