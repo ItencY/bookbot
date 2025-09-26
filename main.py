a2fdb42bca7c637a1f36a688333b406d70b1dec7
ItencY <itency@outlook.com>
2025-09-26 12:27:56 +0300

    add more books and sys.argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def get_book_text(path):
@@ -5,7 +6,11 @@ def get_book_text(path):
         return f.read()
     
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
